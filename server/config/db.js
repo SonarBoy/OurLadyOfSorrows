eea8b33dbcd4ff64a743f53966e0f0627c460511
SonarBoy <Joshua.L99.C@gmail.com>
2020-07-16 22:07:27 -0400

    DB fix maybe

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
-    "URI":"mongodb+srv://Joshua:Joshua@rosary-v1okd.mongodb.net/test?retryWrites=true&w=majority",
+    //"URI":"mongodb+srv://Joshua:Joshua@rosary-v1okd.mongodb.net/test?retryWrites=true&w=majority",
+    "URI":"mongodb+srv://Joshua:Joshua@rosary-v1okd.mongodb.net/OurLadyOfSorrows?retryWrites=true&w=majority",
     //"URI":"mongodb://localhost/OurLadyOfSorrows",
     "secret":"EnvironmentVariable"
 }
