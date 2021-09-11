576310d862198809d5cf2885de7fb02a1bb5a47d
SonarBoy <Joshua.L99.C@gmail.com>
2021-09-11 07:40:09 -0400

    Testing 4

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'OurLadyOfSorrows';
 
+  constructor(private router: Router) {
+  }
   
+  ngOnInit() {
+    this.setUpAnalytics();
+}
+
+  setUpAnalytics() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+        .subscribe((event: NavigationEnd) => {
+            gtag('config', '260620216',
+                {
+                    page_path: event.urlAfterRedirects
+                }
+            );
+        });
+  }
 }
